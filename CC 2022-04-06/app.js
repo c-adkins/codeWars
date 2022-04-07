e2c265fa9fa92511ff7f9d24b621b455641d5e6c
c-adkins <adkins.carey@gmail.com>
2022-04-06 21:12:04 -0700

    Add solution to CC 2022-04-06

--- CC 2022-04-06/app.js
+++ CC 2022-04-06/app.js
@@ -2,3 +2,10 @@
 
 // Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
 
+function paperwork(n, m) {
+    if (n < 0 || m < 0){
+      return 0
+    } else {
+      return n*m
+    }
+  }
