4cc6d223e7b3be222d87239a1c68d9a64cc3c5f8
c-adkins <adkins.carey@gmail.com>
2022-04-07 23:03:35 -0700

    add solution to isNDivisibleByXAndY

--- isNDivisibleByXAndY.js
+++ isNDivisibleByXAndY.js
@@ -1,5 +1,10 @@
 // Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
 
+// can be one line solution
 function isDivisible(n, x, y) {
-  
-}
+    let ans = false;
+    if (n % x === 0 && n % y === 0) {
+      ans = true;
+    }
+    return ans;
+  }
