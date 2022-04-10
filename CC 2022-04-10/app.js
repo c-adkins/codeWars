bf4ed845904a7c8daf9872ebba4e13a334782982
c-adkins <adkins.carey@gmail.com>
2022-04-10 14:08:35 -0700

    Add solution CC 2022-04-10

--- CC 2022-04-10/app.js
+++ CC 2022-04-10/app.js
@@ -1,1 +1,9 @@
 // square every digit of a number and concatenate them.
+
+function squareDigits(num){
+    num = num.toString()
+    num = num.split('')
+    num = num.map(function(x) { return x*x;}) 
+    num = num.join('')
+    return Number(num)
+  }
