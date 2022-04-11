a8ea8f67adf2d73b2aeac4a3bd5784a3edd2a62d
c-adkins <adkins.carey@gmail.com>
2022-04-11 09:59:26 -0700

    Add solution to handle undefined arrays

--- CC 2022-04-08/app.js
+++ CC 2022-04-08/app.js
@@ -2,7 +2,19 @@
 
 // Note: Empty arrays should return 0.
 
+// This solution does not handle empty or undefined arays
 function find_average(array) {
     let avg = array.reduce((a,b) => a + b) /array.length;
     return avg
   }
+
+// Solution now handles undefined or empty arrays
+function find_average(array) {
+  // your code here
+  if (array == null || array.length == 0){
+    return 0;
+  } else {
+    let avg = array.reduce((a,b) => a + b) /array.length;
+    return avg
+  }
+}
