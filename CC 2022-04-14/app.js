9d970ef1a3364fd08b28ee41b4b10df4755b96d7
c-adkins <adkins.carey@gmail.com>
2022-04-14 09:42:29 -0700

    Add successful attempt to sovle

--- CC 2022-04-14/app.js
+++ CC 2022-04-14/app.js
@@ -4,6 +4,7 @@
 
 // If the input is an empty array or is null, return an empty array.
 
+// Did not pass. Count of positive is counting all loops instead of all ints > than 0
 function countPositivesSumNegatives(input) {
     let sum = 0;
     let count = 0;
@@ -11,6 +12,18 @@ function countPositivesSumNegatives(input) {
       if (input[i] < 0){
         sum += input[i]
       } else {
+        count += 1
+      }
+    }return [count,sum]
+}
+
+function countPositivesSumNegatives(input) {
+    let sum = 0;
+    let count = 0;
+    for (let i = 0; i < input.length; i++){
+      if (input[i] < 0){
+        sum += input[i]
+      } else if(input[i] > 0) {
         count += 1
       }
     }return [count,sum]
