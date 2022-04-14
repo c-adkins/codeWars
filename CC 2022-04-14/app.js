be04d10a06835af982efc450df287fb6804ec7a6
c-adkins <adkins.carey@gmail.com>
2022-04-14 09:40:20 -0700

    Add first attempt to solve

--- CC 2022-04-14/app.js
+++ CC 2022-04-14/app.js
@@ -3,3 +3,15 @@
 // Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
 
 // If the input is an empty array or is null, return an empty array.
+
+function countPositivesSumNegatives(input) {
+    let sum = 0;
+    let count = 0;
+    for (let i = 0; i < input.length; i++){
+      if (input[i] < 0){
+        sum += input[i]
+      } else {
+        count += 1
+      }
+    }return [count,sum]
+}
