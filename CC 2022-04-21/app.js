64388f13357800dbf02693a0d04284e59ab60faf
c-adkins <adkins.carey@gmail.com>
2022-04-21 09:31:05 -0700

    Add first CC 22022-04-21 solution

--- CC 2022-04-21/app.js
+++ CC 2022-04-21/app.js
@@ -1,1 +1,12 @@
 // Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+function fakeBin(x){
+    let nums = x.split('')
+    for (let i = 0; i <= nums.length; i++){
+      if (nums[i] < 5){
+        nums[i] = 0
+      } else if (nums[i] >= 5) {
+        nums[i] = 1
+      }
+    } return nums.join("")
+  }
