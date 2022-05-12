f70a49a682f3562035a867b5fcd76875b56a526a
c-adkins <adkins.carey@gmail.com>
2022-05-12 16:07:27 -0700

    Add CC 2022-05-12

---
+++ CC 2022-05-12/app.js
@@ -0,0 +1,14 @@
+function rentalCarCost(d) {
+    // Your solution here
+    let cost = 0
+    if (d >= 7){
+      cost = (d *40) -50
+      return cost
+    } else if (d >= 3){
+      cost = (d*40) -20
+      return cost
+    } else {
+      cost = d *40
+      return cost
+    }
+  }
