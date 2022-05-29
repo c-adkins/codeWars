7627eb0fecc000d3453eb95c6d738ab32fb08e69
c-adkins <adkins.carey@gmail.com>
2022-05-29 14:47:43 -0700

    Add PREP notes

--- CC 2022-04-06/app.js
+++ CC 2022-04-06/app.js
@@ -2,6 +2,10 @@
 
 // Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
 
+// P - number of classmates and number of pages
+// R - 0 if n or m is less than 0 else n*m
+// E - 0, 3 -> 0
+// P - check if n or m is less than 0, if not then return n*m
 function paperwork(n, m) {
     if (n < 0 || m < 0){
       return 0
