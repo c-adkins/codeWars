2cb92508d20cd2796186a1f65b4a5c3f79584dac
c-adkins <adkins.carey@gmail.com>
2022-06-03 14:42:16 -0700

    Add PREP notes

--- CC 2022-04-25/app.js
+++ CC 2022-04-25/app.js
@@ -1,5 +1,9 @@
 // In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 
+// P - string with characters for dna
+// R - the RNA side where A becomes T, T becomes A, C becomes G and G becomes C
+// E - TAGC - ATCG
+// P - create letter map and run for loop of each letter, join the array into a strin
 function DNAStrand(dna) {
     let letters = {'C': 'G', 'G': 'C','A': 'T', 'T': 'A'};
     let complementarySide = [];
