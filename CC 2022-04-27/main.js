0431ed7310187b3497cc8b06521db54d7e767ba3
c-adkins <adkins.carey@gmail.com>
2022-06-09 14:38:59 -0700

    Add PREP notes

--- CC 2022-04-27/main.js
+++ CC 2022-04-27/main.js
@@ -1,5 +1,9 @@
 // Build a function that returns an array of integers from n to 1 where n > 0
 
+// p - starting point n
+// r - array of integerers from n to 1
+// e - 
+// p - Array from n to 1 and reverse the array
 const reverseSeq = n => {
     let arr = Array.from({length:n}, (x,i) => i + 1);
     arr = arr.reverse()
