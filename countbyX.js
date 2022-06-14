22b5561a77f97ee701ba57ff76bfd1c8409dbdd4
c-adkins <adkins.carey@gmail.com>
2022-06-14 16:15:04 -0700

    Add PREP notes

--- countbyX.js
+++ countbyX.js
@@ -4,6 +4,9 @@
 
 // Return the results as an array
 
+// P - number to stop at and number to count by
+// R - an array of each step from 0 to x counting by n
+// E - 25, 5 -> [5,10,15,20,25]
 function countBy(x, n) {
     let z = [];
     for (let step = x; step <= n*x ; step+= x){
