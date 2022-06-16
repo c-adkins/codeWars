9076f37003f064d16dcf078399ad1c5366747de0
c-adkins <adkins.carey@gmail.com>
2022-06-16 14:16:16 -0700

    Add PREP notes

--- enoughSpace.js
+++ enoughSpace.js
@@ -5,7 +5,10 @@
 // wait is the number of people waiting to get on to the bus excluding the driver.
 // If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
 
-
+// P - integers for capcity, currntly on the bus, and waiting at the stop
+// r - 0 if everyone can get on the bus else number of waiting people that will not be able to get on
+// E - c 100, o 50, w 2 -> 0 , c 100, o 100, w 10 -> 10
+// P - if c - o > w then 0, else wait + on - cap
 
 function enough(cap, on, wait) {
     if ((cap - on) >= wait){
