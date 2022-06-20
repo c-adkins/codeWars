d81c9a9ecf3f5fb3c7a73d39afeb6a5166dfb594
c-adkins <adkins.carey@gmail.com>
2022-06-20 14:23:42 -0700

    Add PREP notes

--- removeExclamationMarks.js
+++ removeExclamationMarks.js
@@ -1,5 +1,9 @@
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
+// P - string
+// R - same string without exclamations
+// E - red! -> red
+// P - replace ! with ""
 function removeExclamationMarks(s) {
     return s.replace('!',"");
   }
