58cd93758bee1c4e057819d81fb4853be1ca548b
c-adkins <adkins.carey@gmail.com>
2022-06-21 14:24:49 -0700

    Add PREP notes

--- sentenceSmash.js
+++ sentenceSmash.js
@@ -1,5 +1,10 @@
 // Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. 
 
+// P array of words
+// R one string without spaces
+// E hello there -> hellothere
+// P join the string on spaces
+
 function smash (words) {
     return words.join(" ")
  };
