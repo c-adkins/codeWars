4978bd8b5b24424a04c5a96b7ed959cb44287182
c-adkins <adkins.carey@gmail.com>
2022-06-24 14:27:58 -0700

    Add PREP notes

--- thirdAngleOfTriangle.js
+++ thirdAngleOfTriangle.js
@@ -4,6 +4,11 @@
 
 // Note: only positive integers will be tested.
 
+// p 2 integers for angles of triangle
+// r integer for angle of 3rd
+// e 45, 45 -> 90
+// p subract angles from 180 and return remainder
+
 function otherAngle(a, b) {
     return 180-a-b;
   }
