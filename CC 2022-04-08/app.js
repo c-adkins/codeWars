a2a814259c98da5f20791dbbae25a8c61fcbac3a
c-adkins <adkins.carey@gmail.com>
2022-06-29 14:44:46 -0700

    Add PREP notes

--- CC 2022-04-08/app.js
+++ CC 2022-04-08/app.js
@@ -2,6 +2,11 @@
 
 // Note: Empty arrays should return 0.
 
+// P - array of numbers
+// R - average of the numbers in the array
+// E - 10, 20, 30 -> 30
+// P - reduce by adding each number in the array together, divide sum by array length, return number
+
 // This solution does not handle empty or undefined arays
 function find_average(array) {
     let avg = array.reduce((a,b) => a + b) /array.length;
