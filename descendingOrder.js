6a73c5e0ed53da4a18dd1665ee6f463008c4ac62
c-adkins <adkins.carey@gmail.com>
2022-07-05 15:55:06 -0700

    Add PREP notes

--- descendingOrder.js
+++ descendingOrder.js
@@ -1,7 +1,10 @@
 
 // Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 
-
+// P - non negative interger
+// R - a string or interger in descending order
+// E - 345 -> 543
+// P - convert integer to string, split characters, sort, re join
 function descendingOrder(n){
     //convert integer to string
     let str = n.toString()
