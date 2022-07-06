9826d3b08be45f5aa633fa43b040877ecc61569e
c-adkins <adkins.carey@gmail.com>
2022-07-06 15:57:50 -0700

    Add PREP notes

--- return_negative.py
+++ return_negative.py
@@ -2,5 +2,10 @@
 # for any given number
 # some numbers can be negative
 
+# P - a real number
+# R - a negative number or 0 
+# E - 0 -> 0, 1 -> -1, -0.89 -> -0.89, 45 -> -45
+# P - find absolute value and then return negative absolute value or absolute * -1
+
 def make_negative( number ):
     return -abs(number)
