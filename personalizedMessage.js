a03e1e506130e4bec61ac9ea1aacc824b164d63d
c-adkins <adkins.carey@gmail.com>
2022-07-07 16:01:08 -0700

    Add PREP notes

--- personalizedMessage.js
+++ personalizedMessage.js
@@ -5,3 +5,8 @@ function greet (name, owner) {
       return "Hello guest"
     }
   }
+
+  // P - string for name and string for owner
+  // R - string of Hello boss if owner = name or string Hello guest otherwise. Hello is capital. inputs are returned as they are.
+  // E - bob, bob -> Hello boss, bob, gary -> Hello guest
+  // P - if name = owner return Hello Boss, esle return Hello guest
