918cd6c9276765e9778c69345c836613cd05ce22
c-adkins <adkins.carey@gmail.com>
2022-07-10 16:09:11 -0700

    Add PREP notes

--- calculateBMI.js
+++ calculateBMI.js
@@ -1,5 +1,7 @@
 // Write function bmi that calculates body mass index (bmi = weight / height2).
 
+//P - weight and height as numbers
+//R
 // if bmi <= 18.5 return "Underweight"
 
 // if bmi <= 25.0 return "Normal"
@@ -7,7 +9,8 @@
 // if bmi <= 30.0 return "Overweight"
 
 // if bmi > 30 return "Obese"
-
+// E - w = 180 h = 60 -> "Underweight"
+// P - find bmi, check which categeory it falls under
 function bmi(weight, height) {
     let bmiNum = weight / height**2;
     if (bmiNum <= 18.5){
